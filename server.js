cbae35249577319e0cff4d8866ba4506a1ea8762
maikeru2020 <maikeru2020@gmail.com>
2019-08-22 16:53:49 -0700

    images styling

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const app = express();
+var $ = require('jquery');
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render('home.ejs');
-});
+    res.render('index.ejs', {page: "home"})
+})
 
 app.get('/:page', (req, res) => {
     var page = req.params.page;
-    res.render(page + ".ejs");
+    res.render('index.ejs', {page: page});
 });
 
 const port = process.env.PORT || 8000;
