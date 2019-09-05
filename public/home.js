c13d7fee0b6a0ae20d1da433edba559a953ea7f8
maikeru2020 <maikeru2020@gmail.com>
2019-09-05 14:04:23 -0700

    responsive divs

--- public/home.js
+++ public/home.js
@@ -24,10 +24,5 @@ function slideShow() {
 }
 
 function toggleMap() {
-    if ($("#map").html() == "") {
-        var mapHtml = "<iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d993.1557909477664!2d-4.013597870864654!3d5.321330535083968!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xfc1eb48e7418669%3A0xf4cf652607ae6fe1!2sResidence%20Horizon!5e0!3m2!1sen!2sus!4v1566945296972!5m2!1sen!2sus' width='' height='' frameborder='0' style='border:0;' allowfullscreen=''></iframe>";
-        $("#map").html(mapHtml);
-    } else {
-        $("#map").html("");
-    }
+    $("#map").toggle();
 }
--- public/index.js
+++ public/index.js
@@ -16,5 +16,5 @@ function toggleSidebar() {
 }
 
 $(document).ready(function() {
-    
+   
 });
